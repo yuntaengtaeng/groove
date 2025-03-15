db6a6370057638dd1edec3805eaf8181ae25d67f
yuntaengtaeng <k76253218@gmail.com>
2025-03-15 23:13:45 +0900

    feat : store setComment 특정 id의 comment를 추가하는 함수 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,11 +4,33 @@ import { Short } from '../types';
 interface Store {
   shortList: Short[];
   setShortList: (shorts: Short[]) => void;
+  setComment: (id: string, comment: string) => void;
 }
 
 const useStore = create<Store>((set) => ({
   shortList: [],
   setShortList: (shorts: Short[]) => set({ shortList: shorts }),
+  setComment: (id: string, comment: string) =>
+    set((state) => {
+      const updatedShortList = state.shortList.map((short) => {
+        if (short.id === id) {
+          return {
+            ...short,
+            comments: [
+              ...short.comments,
+              {
+                id: `c${String(short.comments.length + 1).padStart(3, '0')}`,
+                author: '테스터',
+                content: comment,
+                createdAt: new Date(),
+              },
+            ],
+          };
+        }
+        return short;
+      });
+      return { shortList: updatedShortList };
+    }),
 }));
 
 export default useStore;
