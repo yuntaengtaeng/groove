ea3b097f42bfc211fff8bafc9417554f9905a795
yuntaengtaeng <k76253218@gmail.com>
2025-03-15 15:25:45 +0900

    add : ProfileImg 컴포넌트 추가

---
+++ src/components/ProfileImg.tsx
@@ -0,0 +1,20 @@
+import { Image } from 'react-native';
+
+type Props = {
+  url: string;
+};
+
+const ProfileImg = ({ url }: Props) => {
+  return (
+    <Image
+      source={{ uri: url }}
+      style={{
+        width: 36,
+        height: 36,
+        borderRadius: 36,
+      }}
+    />
+  );
+};
+
+export default ProfileImg;
