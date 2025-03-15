d03a220f376bcb9a36e048953a92a9cbc231a36c
yuntaengtaeng <k76253218@gmail.com>
2025-03-15 15:25:30 +0900

    feat : App Wrap 컴포넌트 SafeAreaProvider 추가
    - 하위 컴포넌트들에서 useSafeAreaInsets를 사용하기 위해 추가

--- App.tsx
+++ App.tsx
@@ -1,32 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 import { RootStackParamList } from './src/navigation';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import List from './src/navigation/screens/List';
 import Detail from './src/navigation/screens/Detail';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="List">
-          <Stack.Screen name="List" component={List} />
-          <Stack.Screen name="Detail" component={Detail} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <SafeAreaProvider>
+      <View style={{ flex: 1 }}>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="List">
+            <Stack.Screen name="List" component={List} />
+            <Stack.Screen name="Detail" component={Detail} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
